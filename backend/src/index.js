097c6978a64d79f2060abd40c7329cac53eeabbd
Astha Niharika <asthaniharika123@gmail.com>
2025-02-19 12:04:49 +0530

    updated cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -19,6 +19,12 @@ connectDB();
 
 const PORT = process.env.PORT || 5000;
 
+app.get('/', (req, res) => {
+    res.json({
+      message: 'Hello',
+    });
+  });
+  
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
