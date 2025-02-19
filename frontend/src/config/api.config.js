7ab032e66e82336baa6a4c676667a91955532b4f
Astha Niharika <asthaniharika123@gmail.com>
2025-02-19 11:33:56 +0530

    added cors

--- frontend/src/config/api.config.js
+++ frontend/src/config/api.config.js
@@ -1,12 +1,12 @@
 
 const API_CONFIG = {
-    BASE_URL: 'http://localhost:5000',  
-    ENDPOINTS: {
-      LOGIN: '/login',
-      SIGNUP: '/signup',
-      TASK: '/tasks',
-    },
-    TOKEN_KEY: 'auth_token'  
-  };
-  
-  export default API_CONFIG;
+  BASE_URL: process.env.REACT_APP_API_URL || 'http://localhost:5000',
+  ENDPOINTS: {
+    LOGIN: '/login',
+    SIGNUP: '/signup',
+    TASK: '/tasks',
+  },
+  TOKEN_KEY: 'auth_token'
+};
+
+export default API_CONFIG;
